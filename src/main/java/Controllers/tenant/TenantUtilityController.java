cb1580d53a152d8b600974e934f9b416aebea95e
Y Bui <buinhuy312@gmail.com>
2026-03-05 14:14:25 +0700

    YBN_TenantUtilityController for viewing utilities

--- src/main/java/Controllers/tenant/TenantUtilityController.java
+++ src/main/java/Controllers/tenant/TenantUtilityController.java
@@ -20,7 +20,7 @@ import java.util.List;
  * @author Bui Nhu Y
  */
 @WebServlet(name = "TenantUtilitiesController", urlPatterns = {"/tenant/utility"})
-public class TenantUtilitiesController extends HttpServlet {
+public class TenantUtilityController extends HttpServlet {
 
     /**
      * Processes requests for both HTTP <code>GET</code> and <code>POST</code>
